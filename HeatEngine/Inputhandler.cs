c3d6cd17affff92ad43bf550ac7bc043cfbd43e9
akay2 <akaycantezcan@hotmail.de>
2015-02-15 18:11:00 +0100

    Akay: InputHandler erweitert und fürs erste Fertig gestellt

--- HeatEngine/Inputhandler.cs
+++ HeatEngine/Inputhandler.cs
@@ -12,12 +12,144 @@ using Microsoft.Xna.Framework.Media;
 
 namespace HeatEngine
 {
-    public class InputHandler: Microsoft.Xna.Framework.GameComponent
+    public class InputHandler : Microsoft.Xna.Framework.GameComponent
     {
-        public InputHandler(Game game)
+        #region Field Region
+
+        //tastatur
+        static KeyboardState keyboardState; //der status der Tastatur
+        static KeyboardState lastKeyboardState; //der letzte status der Tastatur
+
+        //gamePad
+        static GamePadState[] gamePadState; //der status des Gamepads
+        static GamePadState[] lastGamePadState; //der letzte status des GamePads
+
+        #endregion
+
+
+
+
+        #region Properties Region
+
+        public static KeyboardState KeyboardState
+        {
+            get { return keyboardState; } // gibt den aktuellen wert der Tastatur wider
+        }
+
+        public static KeyboardState LastKeyboardState
+        {
+            get { return lastKeyboardState; } // gibt den letzten wert der tastatur wider
+        }
+
+        public static GamePadState[] GamePadState
+        {
+            get { return gamePadState; } //gibt den status des GamePads wider
+        }
+
+        public static GamePadState[] LastGamePadState
+        {
+            get { return lastGamePadState; } //gibt den letzten stand des GamePads wider
+        }
+
+        #endregion
+
+
+
+
+        #region Constructor Region
+
+        public InputHandler(Game game) //contructor mit einem argument
             : base(game)
         {
+            keyboardState = Keyboard.GetState(); //setzt den keyboardwert für den aktuellen zustand fest
 
+            gamePadState = new GamePadState[Enum.GetValues(typeof(PlayerIndex)).Length]; //setted alle möglichen spieler Index' fest
+
+            foreach (PlayerIndex index in Enum.GetValues(typeof(PlayerIndex)))  //geht durch alle gefundenen Gamepads durch
+                gamePadState[(int)index] = GamePad.GetState(index); //setzt den entsprechenden status dem entsprechenden gamePad zu
         }
+
+        #endregion
+
+
+
+
+        #region Standart Methods Region
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }   //initialisiert die Datei
+
+        public override void Update(GameTime gameTime)
+        {
+            lastKeyboardState = keyboardState; //setzt den letzten keyboardstate auf den des aktuellen
+            keyboardState = Keyboard.GetState(); //aktualisiert den akutellen zustand der tastatur
+
+            lastGamePadState = (GamePadState[])gamePadState.Clone(); //Clont den aktuellen gamepadState und castet es auf das letzte gamepadState
+            foreach (PlayerIndex index in Enum.GetValues(typeof(PlayerIndex)))  //geht durch alle gefundenen Gamepads durch
+                gamePadState[(int)index] = GamePad.GetState(index); //setzt den entsprechenden status dem entsprechenden gamePad zu
+
+            base.Update(gameTime);
+        } //update wird einmal pro frame ausgeführt
+
+        #endregion
+
+
+
+
+        #region Methods Region
+
+        public static void Flush()
+        {
+            lastKeyboardState = keyboardState; //löscht die tastaturänderung zum letzten frame
+            //am besten beim übergang per tasatendruck anwenden
+        }
+
+        #endregion
+
+
+
+
+        #region Keyboard Region
+
+        public static bool KeyReleased(Keys key)
+        {
+            return keyboardState.IsKeyUp(key) && lastKeyboardState.IsKeyDown(key); //returns True wenn eine taste welche im letzten Frame gedrückt war nun losgelassen wurde
+        }
+
+        public static bool KeyPressed(Keys key)
+        {
+            return keyboardState.IsKeyDown(key) && lastKeyboardState.IsKeyUp(key); //returns True wenn eine taste gedrückt wird welche im letzten frame ungedrückt war
+        }
+
+        public static bool KeyDown(Keys key)
+        {
+            return keyboardState.IsKeyDown(key); //returns True wenn eine taste gedrückt wird
+        }
+
+        #endregion
+
+
+
+
+        #region GamePad Region
+
+        public static bool ButtonReleased(Buttons button, PlayerIndex index)
+        {
+            return gamePadState[(int)index].IsButtonDown(button) && lastGamePadState[(int)index].IsButtonUp(button); //returns True wenn eine taste logelassen wird welche gedrückt war
+        }
+
+        public static bool ButtonPressed(Buttons button, PlayerIndex index)
+        {
+            return gamePadState[(int)index].IsButtonUp(button) && lastGamePadState[(int)index].IsButtonDown(button);    //returns True wenn eine taste gedrückt wird welche noch nicht gedrückt war
+        }
+
+        public static bool ButtonDown(Buttons button, PlayerIndex index)
+        {
+            return gamePadState[(int)index].IsButtonDown(button); //returns true wenn taste gedrückt wird
+        }
+
+        #endregion
     }
 }
--- HeatWarrior/HeatWarrior/Game1.cs
+++ HeatWarrior/HeatWarrior/Game1.cs
@@ -1,6 +1,9 @@
+//system
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
+//XNA
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -8,13 +11,14 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+
+//Heat Engine
 using HeatEngine;
 
 namespace HeatWarrior
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+    
+
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
@@ -24,14 +28,12 @@ namespace HeatWarrior
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            //Hier werden die Componenten Hinzugefügt
+            Components.Add(new InputHandler(this)); //Dieses Komponent prüft dauerhaft auf eingaben der tastetur/GamePad
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+       
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -39,10 +41,7 @@ namespace HeatWarrior
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+       
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -51,20 +50,13 @@ namespace HeatWarrior
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
+       
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -76,10 +68,7 @@ namespace HeatWarrior
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
