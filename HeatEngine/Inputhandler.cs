8dca1a5eef506c7ae890d726f6a9c084765d21c7
Luckywinner <lucas.damrau@gmail.com>
2015-02-15 17:38:39 +0100

    Lucas: add InputHandler

--- HeatEngine/Inputhandler.cs
+++ HeatEngine/Inputhandler.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace HeatEngine
 {
-    public class Inputhandler
+    public class InputHandler: Microsoft.Xna.Framework.GameComponent
     {
+        public InputHandler(Game game)
+            : base(game)
+        {
+
+        }
     }
 }
